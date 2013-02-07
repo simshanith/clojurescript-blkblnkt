f19985ebf282f21f593a4170c7a5e98fc9672482
Shane Daniel <sdaniel@vml.com>
2013-02-07 03:40:55 -0600

    gitignore update

--- src/sim_cljs/server.clj
+++ src/sim_cljs/server.clj
@@ -6,7 +6,12 @@
             [net.cgrand.enlive-html :as enlive]
             [cljs.repl.browser]
             [cemerick.piggieback]
-            [watchtower.core :as watch])
+            [cemerick.drawbridge :as drawbridge]
+            [ring.middleware.params :as params]
+            [ring.middleware.keyword-params :as keyword-params]
+            [ring.middleware.nested-params :as nested-params]
+            [ring.middleware.session :as session]
+            [ring.middleware.basic-authentication :as basic])
   (:gen-class))
 
 (enlive/deftemplate layout "sim_cljs/views/layout.html" [])
@@ -26,10 +31,25 @@
   (cemerick.piggieback/cljs-repl
   :repl-env (doto (cljs.repl.browser/repl-env :port 9000)
               cljs.repl/-setup)))
-;;
-(defn -main [& args]
-  (jetty/run-jetty #'app {:port 3000}))
 
-(watch/watcher ["src/sim_cljs/views/"]
-               (watch/rate 250)
-               (watch/on-change (fn [_] (require `sim-cljs.server :reload))))
+(def drawbridge-handler
+  (-> (cemerick.drawbridge/ring-handler)
+      (keyword-params/wrap-keyword-params)
+      (nested-params/wrap-nested-params)
+      (params/wrap-params)
+      (session/wrap-session)))
+
+(defn authenticated? [name pass]
+  (= [name pass] [(System/getenv "AUTH_USER") (System/getenv "AUTH_PASS")]))
+
+(defn wrap-drawbridge [handler]
+  (fn [req]
+    (let [handler (if (= "/repl" (:uri req))
+                    (basic/wrap-basic-authentication
+                     drawbridge-handler authenticated?)
+                    handler)]
+      (handler req))))
+
+(defn -main [& [port]]
+  (let [port (Integer. (or port (System/getenv "PORT")))]
+    (jetty/run-jetty (wrap-drawbridge app) {:port port :join? false})))
